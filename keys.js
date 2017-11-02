78b312a8d42529b92e13a02eacfaf24253edf64b
William Brooks <brooks.william@gmail.com>
2017-11-02 11:58:13 -0700

    skeleton files

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'YCa6B71O1UPvI4MeTuDVH64ir',
+  consumer_secret: 'QFCl8nBWRqtV8GK0URTZ7PAr73QwLmZEGqbq0LyRKagsXeyZRE',
+  access_token_key: '926158963241975808-ZGl0ynxRB6gVrsorhq89UdXawXuGIGs',
+  access_token_secret: 'cUeSB8JLuRVoJpwFQtY469McZ1VovO7vYjLy1bD1ozI7v',
+}
+
+module.exports = twitterKeys;
